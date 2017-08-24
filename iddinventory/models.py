308217f6939803b80eff697e0ddc8a97823c69cd
Mike Molenda <molenda.mike@gmail.com>
2017-08-24 10:26:32 -0500

    Start setup of model

--- iddinventory/models.py
+++ iddinventory/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    manufacturer = models.CharField(max_length=200)
+    model_no = models.CharField(max_length=200)
+    description = models.CharField(max_length=200, blank=True, null=True)
+    image = models.ImageField(upload_to='../images/items', blank=True, null=True)
+
+class Asset(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+
+
