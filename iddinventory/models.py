2b6748c4706eec5406a0f4c05808d84f6473938c
Mike Molenda <molenda.mike@gmail.com>
2017-08-24 10:26:55 -0500

    Correct spacing error

--- iddinventory/models.py
+++ iddinventory/models.py
@@ -7,7 +7,9 @@ class Product(models.Model):
     description = models.CharField(max_length=200, blank=True, null=True)
     image = models.ImageField(upload_to='../images/items', blank=True, null=True)
 
+
 class Asset(models.Model):
     product = models.ForeignKey(Product, on_delete=models.PROTECT)
+
 
 
