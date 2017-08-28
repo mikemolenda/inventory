fc4c0705b1a1de0fa234a2a94d3b7c0747d8cf4e
Mike Molenda <molenda.mike@gmail.com>
2017-08-28 08:07:05 -0500

    Add missing comma

--- inventory/settings.py
+++ inventory/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'idd_inventory.apps.IddInventoryConfig'
+    'idd_inventory.apps.IddInventoryConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
