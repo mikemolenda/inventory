f053e769346746200fff8393accaaab082a33252
Mike Molenda <molenda.mike@gmail.com>
2017-08-28 08:58:05 -0500

    Update name of SQLite DB

--- inventory/settings.py
+++ inventory/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'inventory.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'iddinventory.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'idd_inventory.sqlite3'),
     }
 }
 
