124691bac69271ef0fc4892536bc1470074adc4d
Mike Molenda <molenda.mike@gmail.com>
2017-08-28 08:09:36 -0500

    Change max_length attribute name to max_digits

--- idd_inventory/models.py
+++ idd_inventory/models.py
@@ -17,7 +17,7 @@ class Asset(models.Model):
     vendor = models.ForeignKey(Vendor, on_delete=models.PROTECT, blank=True, null=True)
     vendor_item_no = models.CharField(max_length=100, blank=True, null=True)
     acquire_date = models.DateField(blank=True, null=True)
-    purchase_price = models.DecimalField(max_length=8, decimal_places=2, blank=True, null=True)
+    purchase_price = models.DecimalField(max_digits=8, decimal_places=2, blank=True, null=True)
     serial_no = models.CharField(max_length=100, blank=True, null=True)
     asset_tag_no = models.CharField(max_length=100, blank=True, null=True)
     capital_equipment = models.BooleanField()
